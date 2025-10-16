fd6439d277a0bc01e74065c47d6fb6d7a875cf1c
masarnovsky <m.masarnovsky@gmail.com>
2025-10-16 10:07:20 +0200

    add forced portrait mode for MainActivity

--- app/src/main/java/com/masarnovsky/big/mvvm/view/MainActivity.kt
+++ app/src/main/java/com/masarnovsky/big/mvvm/view/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.masarnovsky.big.mvvm.view
 
 import android.content.Intent
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -41,6 +42,9 @@ import com.masarnovsky.big.mvvm.viewmodel.MainViewModel
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
         setContent {
             FullscreenTextTheme {
                 MainScreen(
